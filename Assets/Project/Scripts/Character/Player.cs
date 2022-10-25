a39c63c65817b8730591496af1074d1224fc7e80
Abel <abelpujol99@gmail.com>
2022-10-25 15:11:49 +0200

    Player Movement

---
+++ Assets/Project/Scripts/Character/Player.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Scripts.ProjectPhysics2D;
+using Unity.VisualScripting;
+
+namespace Project.Scripts.Character
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        
+        [SerializeField] private ProjectPhysics2D.ProjectPhysics2D _physics2D;
+
+        private float _currentSpeed = 4;
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            Controls();
+        }
+
+        private void FixedUpdate()
+        {
+            Movement();
+        }
+        
+        private void Movement()
+        {
+            float movementX = 0;
+            float movementY = 0;
+            
+            if (Input.GetKey(KeyCode.A))
+            {
+                movementX = -1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                movementX = 1;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                movementY = 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                movementY = -1;
+            }
+
+            Vector2 movementDirection = new Vector3(movementX, movementY).normalized * _currentSpeed;
+            
+            _physics2D.Move(transform, movementDirection);
+        }
+
+        private void Controls()
+        {
+            
+        }
+    }
+}
+
