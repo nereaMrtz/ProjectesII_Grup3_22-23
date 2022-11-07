3483d397a109b514c1a69412aa6a1e033e68d570
Juls <julia.martos@enti.cat>
2022-11-07 21:00:06 +0100

    Exit button

---
+++ Assets/Project/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void PlayButton() { }
+
+    public void OptionsButton() { }
+
+    public void CreditsButton() { }
+
+    public void ExitButton() {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
