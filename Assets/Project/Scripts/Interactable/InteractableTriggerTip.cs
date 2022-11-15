abec7e9f489ed0dac4e046105eb470f51c653d3b
Abel Pujol <abelpujol99@gmail.com>
2022-11-15 18:06:01 +0100

    Tip System Changed and Tested

---
+++ Assets/Project/Scripts/Interactable/InteractableTriggerTip.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Scripts.UI;
+
+namespace Project.Scripts.Interactable 
+{
+    public class InteractableTriggerTip : MonoBehaviour
+    {
+
+        private const int PLAYER_LAYER = 6;
+
+        [SerializeField] private ObjectTip _tipText;
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == PLAYER_LAYER)
+            {
+                _tipText.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == PLAYER_LAYER)
+            {
+                _tipText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Project/Scripts/UI/ObjectTip.cs
@@ -0,0 +1,79 @@
+using System;
+using TMPro;
+using System.Collections;
+using Project.Scripts.Managers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Project.Scripts.UI
+{
+    public class ObjectTip : MonoBehaviour
+    {
+        public static Action<string, Vector2> OnView;
+
+        public static Action OffView;
+
+        [SerializeField] private RectTransform _hoverTip;
+
+        [SerializeField] private TextMeshProUGUI _tip;
+
+        [SerializeField] private float OffsetY;
+        [SerializeField] private float OffsetX;
+
+        [SerializeField] private GameObject _gameObjectAttached;
+
+        [SerializeField] private String _tipText;
+        [SerializeField] private String _longText;
+
+        [SerializeField] private bool _activateHover;
+
+        private bool _onView;        
+
+        private void Start()
+        {
+            HideText();
+        }
+
+        private void Update()
+        {
+            if (_activateHover)
+            {
+                ShowText(_tipText, _gameObjectAttached.transform.position);
+            }
+        }
+
+        /*private void OnEnable()
+        {
+            if (_activateHover)
+            {
+                ShowText(_tipText, _gameObjectAttached.transform.position);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_activateHover)
+            {
+                HideText();
+            }
+        }*/
+
+        private void ShowText(string text, Vector2 objectPosition)
+        {
+            _tip.text = text;
+
+            Vector2 sizeDelta = _tip.gameObject.GetComponent<RectTransform>().sizeDelta;
+
+            _hoverTip.sizeDelta = new Vector2(sizeDelta.x > 500 ? 500 : sizeDelta.x, sizeDelta.y);
+            _hoverTip.transform.position = new Vector2(objectPosition.x + OffsetX, objectPosition.y + OffsetY);
+            _hoverTip.gameObject.SetActive(true);
+        }
+
+        private void HideText()
+        {
+            _tip.text = default;
+            _hoverTip.gameObject.SetActive(false);
+        }
+
+    }
+}
