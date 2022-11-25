aaa7185ef3c504684867c0f7a3c9673e996e3620
Nerea <nerea.martinez@enti.cat>
2022-11-25 20:32:58 +0100

    trying to solve

--- Assets/Project/Scripts/FeedbackCircle/Circle.cs
+++ Assets/Project/Scripts/FeedbackCircle/Circle.cs
@@ -9,21 +9,35 @@ namespace Project.Scripts.FeedbackCircle
     public class Circle : MonoBehaviour
     {
         private const string SHOW_INVENTORY = "ShowInventory";
-        [SerializeField] private GameObject biggerCircle;
+        [SerializeField] private GameObject smallCircle;
+        [SerializeField] private BoxCollider2D circleColider;
+        [SerializeField] private GameObject BigCircle;
         [SerializeField] private GameObject quitCircle;
         [SerializeField] private Animator _animator;
 
-        public void ActiveCircle()
+        public void ActiveSmallCircle()
         {
-            biggerCircle.SetActive(!biggerCircle.activeSelf);
+            smallCircle.SetActive(!smallCircle.activeSelf);
+       
         }
+        
+        public void ActiveBigCircle()
+        {
+            BigCircle.SetActive(!BigCircle.activeSelf);
+        }
+        
 
         public void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
-                ActiveCircle();
-                _animator.SetBool(SHOW_INVENTORY, true);
+                ActiveSmallCircle();
+
+               // if ()
+               // {
+                    ActiveBigCircle();
+                    _animator.SetBool(SHOW_INVENTORY, true);
+               // }
             }
         }
 
@@ -31,8 +45,15 @@ namespace Project.Scripts.FeedbackCircle
         {
             if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
-                ActiveCircle();
-                _animator.SetBool(SHOW_INVENTORY, false);
+                ActiveSmallCircle();
+
+                if (smallCircle.layer == LayerMask.NameToLayer("BiggerCircleTrigger"))
+                {
+                    ActiveBigCircle();
+                    _animator.SetBool(SHOW_INVENTORY, true);
+
+
+                }
             }
         }
 
