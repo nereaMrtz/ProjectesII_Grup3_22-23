5dc1e9325ac09d29fe31f5bff33c7a9f81720ed2
Nerea <nerea.martinez@enti.cat>
2022-11-28 13:58:00 +0100

    Interaccion circulos completa
    Falta el script de attach para la E

--- Assets/Project/Scripts/FeedbackCircle/Circle.cs
+++ Assets/Project/Scripts/FeedbackCircle/Circle.cs
@@ -8,36 +8,21 @@ namespace Project.Scripts.FeedbackCircle
 {
     public class Circle : MonoBehaviour
     {
-        private const string SHOW_INVENTORY = "ShowInventory";
         [SerializeField] private GameObject smallCircle;
-        [SerializeField] private BoxCollider2D circleColider;
-        [SerializeField] private GameObject BigCircle;
         [SerializeField] private GameObject quitCircle;
-        [SerializeField] private Animator _animator;
 
         public void ActiveSmallCircle()
         {
             smallCircle.SetActive(!smallCircle.activeSelf);
        
         }
-        
-        public void ActiveBigCircle()
-        {
-            BigCircle.SetActive(!BigCircle.activeSelf);
-        }
-        
-
+  
         public void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
                 ActiveSmallCircle();
-
-               // if ()
-               // {
-                    ActiveBigCircle();
-                    _animator.SetBool(SHOW_INVENTORY, true);
-               // }
+           
             }
         }
 
@@ -46,14 +31,6 @@ namespace Project.Scripts.FeedbackCircle
             if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
                 ActiveSmallCircle();
-
-                if (smallCircle.layer == LayerMask.NameToLayer("BiggerCircleTrigger"))
-                {
-                    ActiveBigCircle();
-                    _animator.SetBool(SHOW_INVENTORY, true);
-
-
-                }
             }
         }
 
