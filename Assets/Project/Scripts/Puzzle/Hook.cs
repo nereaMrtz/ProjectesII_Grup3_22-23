55d84bfa07257837975f1c26568f5659d0ea5031
Júlia <julia.martos@enti.cat>
2022-11-30 14:08:29 +0100

    Puzzle de cuadros done :)

---
+++ Assets/Project/Scripts/Puzzle/Hook.cs
@@ -0,0 +1,9 @@
+using Project.Scripts.Interactable.Static.RequiredInventory.FromInventoryToWorldSpace;
+
+namespace Project.Scripts.Puzzle
+{
+    public class Hook : PlacePickupOnWorldSpace
+    {
+        
+    }
+}
