e407a0131ce7954f00cdd73ed18916b27f62eca1
Nerea <nerea.martinez@enti.cat>
2022-11-30 01:36:54 +0100

    Refactor de comments hecho
    Con animacion, capacidad de varios dialogos y pulsar tecla para pasarlos

---
+++ Assets/Project/Scripts/FeedbackComments/ActivateDialog.cs
@@ -0,0 +1,22 @@
+using Project.Scripts.Interactable.Static.NotRequiredInventory;
+using Project.Scripts.Sound;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDialog : MonoBehaviour {
+
+    public DialogText text;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            FindObjectOfType<DialogController>().ActivateDialogBox(text);
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            FindObjectOfType<DialogController>().CloseDialogBox();
+    }
+}
