91c738cd3936f94007c02a6c8f1bbc57151d181c
Abel <abelpujol99@gmail.com>
2022-11-30 22:50:35 +0100

    Added Doors

--- Assets/Project/Scripts/PuzzleDependentObject/PuzzleDoor.cs
+++ Assets/Project/Scripts/PuzzleDependentObject/PuzzleDoor.cs
@@ -7,11 +7,13 @@ using UnityEngine;
 
 namespace Project.Scripts.PuzzleDependentObject
 {
-    public class PuzzleDoor : UnlockableObject
+    public class PuzzleDoor : MonoBehaviour
     {
         private const String SIMPLE_DOOR_SOUND = "Simple Door Sound";
         private const String SLIDE_SIMPLE_DOOR_SOUND = "Slide Simple Door Sound";
 
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        
         [SerializeField] private PuzzleScript _puzzle;
 
         [SerializeField] private bool _up;
@@ -19,10 +21,14 @@ namespace Project.Scripts.PuzzleDependentObject
         [SerializeField] private bool _right;
         [SerializeField] private bool _left;
 
+        private AudioManager _audioManager;
+
         private Transform _transform;
 
         private float _width;
         private float _height;
+
+        private bool _moved;
 
         private void Start()
         {
@@ -31,46 +37,44 @@ namespace Project.Scripts.PuzzleDependentObject
             _width = spriteRenderSize.x;
             _height = spriteRenderSize.y;
             _transform = transform;
+            _audioManager = FindObjectOfType<AudioManager>();
         }
 
         private void Update()
         {
-            if (_puzzle.GetCompleted())
+            if (_puzzle.GetCompleted() && !_moved)
             {
-                throw new NotImplementedException();
+                StartCoroutine(MoveDoor(_audioManager));
             }
         }
 
-        protected override void Unlock(AudioManager audioManager)
-        {
-            StartCoroutine(MoveDoor(audioManager));
-        }
-
-
         private IEnumerator MoveDoor(AudioManager audioManager)
         {
+            _moved = true;
+            _spriteRenderer.sortingOrder--;
             audioManager.Play(SIMPLE_DOOR_SOUND);
             yield return new WaitForSeconds(audioManager.ClipDuration(SIMPLE_DOOR_SOUND));
             audioManager.Play(SLIDE_SIMPLE_DOOR_SOUND);
-            
+
             Vector3 targetPosition = GetTargetPosition();
 
             float slideSoundDuration = audioManager.ClipDuration(SLIDE_SIMPLE_DOOR_SOUND);
             float distance = Vector2.Distance(_transform.position, targetPosition);
             float slideSpeed = distance / slideSoundDuration;
-            
+
             while (Vector3.Distance(_transform.position, targetPosition) > 0)
             {
-                float speed = slideSpeed * Time.deltaTime; 
+                Debug.Log("hola");
+                float speed = slideSpeed * Time.deltaTime;
                 _transform.position = Vector2.MoveTowards(_transform.position, targetPosition, speed);
                 yield return null;
             }
         }
-        
+
         private Vector3 GetTargetPosition()
         {
             Vector2 targetPosition = default;
-            
+
             if (_right)
             {
                 targetPosition = new Vector2(_transform.position.x + _width, _transform.position.y);
@@ -82,7 +86,7 @@ namespace Project.Scripts.PuzzleDependentObject
             else if (_down)
             {
                 targetPosition = new Vector2(_transform.position.x, _transform.position.y - _height);
-                
+
             }
             else if (_up)
             {
@@ -91,6 +95,6 @@ namespace Project.Scripts.PuzzleDependentObject
 
             return targetPosition;
         }
-        
+
     }
 }
