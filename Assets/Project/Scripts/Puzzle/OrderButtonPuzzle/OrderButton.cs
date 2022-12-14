381272f54f0d9018dda9317e91f40b0fd72eb30c
Abel <abelpujol99@gmail.com>
2022-12-14 12:34:31 +0100

    puzzle button done

---
+++ Assets/Project/Scripts/Puzzle/OrderButtonPuzzle/OrderButton.cs
@@ -0,0 +1,41 @@
+using System;
+using Project.Scripts.Interactable.Static.NotRequiredInventory;
+using Project.Scripts.Sound;
+using UnityEngine;
+
+namespace Project.Scripts.Puzzle.OrderButtonPuzzle
+{
+    public class OrderButton : NotRequiredInventoryInteractable
+    {
+        [SerializeField] private int _pressOrder;
+
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+
+        private bool _pressed;
+        
+        public override void Interact(AudioManager audioManager)
+        {
+            _pressed = true;
+        }
+
+        public void ChangeColor(Color color)
+        {
+            _spriteRenderer.color = color;
+        }
+
+        public int GetPressOrder()
+        {
+            return _pressOrder;
+        }
+
+        public bool IsPressed()
+        {
+            return _pressed;
+        }
+
+        public void SetPressed(bool pressed)
+        {
+            _pressed = pressed;
+        }
+    }
+}
