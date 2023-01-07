ca7710a4b4f3f99343accb5ee4bee6d2ac4675c8
Júlia <julia.martos@enti.cat>
2023-01-07 18:11:16 +0100

    Fix UI in labyrinth puzzle

--- Assets/Project/Scripts/Interactable/Buttons/Button.cs
+++ Assets/Project/Scripts/Interactable/Buttons/Button.cs
@@ -22,8 +22,6 @@ namespace Project.Scripts.Interactable.Buttons
             _actualPanel.SetActive(false);
             _panelToActivate.SetActive(true);
             (_actualPanel, _panelToActivate) = (_panelToActivate, _actualPanel);
-            
-            // Desactivat caminar player
         }
 
         public void ExitButton()
