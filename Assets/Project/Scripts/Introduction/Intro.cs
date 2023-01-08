f7f3dac95b65039e5cd6dd692cdedf2370d9fb65
Júlia <julia.martos@enti.cat>
2023-01-08 16:45:46 +0100

    Fusion of menu, intro and scene .
    Haz lo tuyo amiga ;)

--- Assets/Project/Scripts/Introduction/Intro.cs
+++ Assets/Project/Scripts/Introduction/Intro.cs
@@ -1,90 +1,93 @@
-using Project.Scripts.FeedbackComments;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Intro : MonoBehaviour
+namespace Project.Scripts.Introduction
 {
-    private Queue<string> dialogs = new Queue<string>();
-    public DialogText text;
-    [SerializeField] TextMeshProUGUI screenText;
-    [SerializeField] private GameObject clickSprite;
-
-    bool OnDialog = false;
-    bool writtingText = false;
-    bool onIntro = true;
-
-    private IEnumerator _coroutine;
-    private float time = 0.05f;
-
-    private void Start()
+    public class Intro : MonoBehaviour
     {
-        SetDialog(text);
-    }
+        private Queue<string> dialogs = new Queue<string>();
+        public DialogText text;
+        [SerializeField] TextMeshProUGUI screenText;
+        [SerializeField] private GameObject clickSprite;
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        bool OnDialog = false;
+        bool writtingText = false;
+        bool onIntro = true;
+
+        private IEnumerator _coroutine;
+        private float time = 0.05f;
+
+        private void Start()
         {
-            clickSprite.SetActive(false);
+            SetDialog(text);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                clickSprite.SetActive(false);
+                NextSentence();
+            }
+        }
+
+        public void SetDialog(DialogText objectText)
+        {
+            OnDialog = true;
+            ActivateText();
+            text = objectText;
+        }
+
+        public void ActivateText()
+        {
+            screenText.enabled = true;
+            dialogs.Clear();    //Borro el texto que habia antes en la cola
+            foreach (string saveText in text.textArray) //Me guardo todos los textos de cada array y los meto en la cola
+            {
+                dialogs.Enqueue(saveText);
+            }
             NextSentence();
         }
-    }
+        public void NextSentence()
+        { 
+            if (dialogs.Count == 0)   //Si se ha acabado el dialogo, cerramos el cartel
+            {
+                CloseDialogBox();
+                OnDialog = false;
+                SceneManager.LoadScene(2);
+                return;
+            }
 
-    public void SetDialog(DialogText objectText)
-    {
-        OnDialog = true;
-        ActivateText();
-        text = objectText;
-    }
+            time = 0.05f;
 
-    public void ActivateText()
-    {
-        screenText.enabled = true;
-        dialogs.Clear();    //Borro el texto que habia antes en la cola
-        foreach (string saveText in text.textArray) //Me guardo todos los textos de cada array y los meto en la cola
-        {
-            dialogs.Enqueue(saveText);
-        }
-        NextSentence();
-    }
-    public void NextSentence()
-    { 
-        if (dialogs.Count == 0)   //Si se ha acabado el dialogo, cerramos el cartel
-        {
-            CloseDialogBox();
-            OnDialog = false;
-            return;
+            string actualSentence = dialogs.Dequeue();
+            screenText.text = actualSentence;
+            _coroutine = ShowCharacters(actualSentence);
+            StartCoroutine(_coroutine);
+
         }
 
-        time = 0.05f;
-
-        string actualSentence = dialogs.Dequeue();
-        screenText.text = actualSentence;
-        _coroutine = ShowCharacters(actualSentence);
-        StartCoroutine(_coroutine);
-
-    }
-
-    public void CloseDialogBox()
-    {
-        screenText.text = "";
-        screenText.enabled = false;
-        StopCoroutine(_coroutine);
-    }
-
-    IEnumerator ShowCharacters(string textToShow)
-    {
-        writtingText = true;
-        screenText.text = "";
-        foreach (char character in textToShow.ToCharArray())
+        public void CloseDialogBox()
         {
-            screenText.text += character;
-            yield return new WaitForSeconds(time);
+            screenText.text = "";
+            screenText.enabled = false;
+            StopCoroutine(_coroutine);
         }
-        clickSprite.SetActive(true);
-        writtingText = false;
+
+        IEnumerator ShowCharacters(string textToShow)
+        {
+            writtingText = true;
+            screenText.text = "";
+            foreach (char character in textToShow.ToCharArray())
+            {
+                screenText.text += character;
+                yield return new WaitForSeconds(time);
+            }
+            clickSprite.SetActive(true);
+            writtingText = false;
+        }
     }
 }
