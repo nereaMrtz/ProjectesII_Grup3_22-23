379699810a7170b1575adb08efb2f78c1b607738
Nerea <nerea.martinez@enti.cat>
2023-02-10 19:29:00 +0100

    Salas hiladas

--- Assets/Project/Scripts/Levels/CambiarSala.cs
+++ Assets/Project/Scripts/Levels/CambiarSala.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Sala1_3 : MonoBehaviour
+public class CambiarSala : MonoBehaviour
 {
-
+    [SerializeField] protected int _sala;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-            SceneManager.LoadScene(6);
+            SceneManager.LoadScene(_sala);
     }
 }
