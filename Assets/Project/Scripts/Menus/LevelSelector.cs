8b7b013d3bc2de656ee785dff447f8017a913ccc
Nerea <nerea.martinez@enti.cat>
2023-03-14 20:22:59 +0100

    Level Selector enables only unlocked levels

--- Assets/Project/Scripts/Menus/LevelSelector.cs
+++ Assets/Project/Scripts/Menus/LevelSelector.cs
@@ -1,24 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Project.Scripts.Managers;
+using UnityEngine.UI;
 
 namespace Project.Scripts.Menus
 {
     public class LevelSelector : MonoBehaviour
     {
-        bool[] levels = new bool[30];
-
+        bool[] levels;
+        [SerializeField] Button[] button;
+       
         private void Start()
         {
+            levels = GameManager.Instance.GetLevels();
+            levels[0] = true;
+
             for(int i = 0; i < levels.Length; i++)
             {
-                levels[i] = false;
+                if (!levels[i])
+                {
+                    button[i].interactable = false;
+                }
+                else
+                {
+                    button[i].interactable = true;                 
+                }
             }
         }
-
-        void UnlockLevel(int level)
-        {
-            levels[level] = true;
-        }
-
     }
 }
