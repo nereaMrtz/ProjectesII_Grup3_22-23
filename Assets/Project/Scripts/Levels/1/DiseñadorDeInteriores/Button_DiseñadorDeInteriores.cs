05c2fabb9c889109995e2c21b2a2d8bc1d05d9e7
Abel Pujol <abelpujol99@gmail.com>
2023-03-28 14:00:13 +0200

    Diseñador de interiores hecho
    Queda ajustar las puertas

---
+++ Assets/Project/Scripts/Levels/1/DiseñadorDeInteriores/Button_DiseñadorDeInteriores.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace Project.Scripts.Levels._1.DiseñadorDeInteriores
+{
+    public class Button_DiseñadorDeInteriores : MonoBehaviour
+    {
+        [SerializeField] private Door_DiseñadorDeInteriores _door;
+
+        private bool _clicked;
+
+        private void OnMouseDown()
+        {
+            if (_door.GetClicked())
+            {
+                (transform.position, _door.transform.position) = (_door.GetTeleportButtonPosition(), transform.position - _door.GetOffset());    
+                Destroy(this);
+            }
+
+            _clicked = true;
+        }
+
+        public bool GetClicked()
+        {
+            return _clicked;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Levels/1/DiseñadorDeInteriores/Door_DiseñadorDeInteriores.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+namespace Project.Scripts.Levels._1.DiseñadorDeInteriores
+{
+    public class Door_DiseñadorDeInteriores : MonoBehaviour
+    {
+        [SerializeField] private Button_DiseñadorDeInteriores _button;
+
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+
+        private Vector3 _offset;
+
+        private Vector3 _teleportButtonPosition;
+
+        private bool _clicked;
+
+        private void Start()
+        {
+            _offset = new Vector3(_spriteRenderer.size.x / 2, _spriteRenderer.size.y / 2);
+            _teleportButtonPosition =
+                new Vector3(transform.position.x + _offset.x, transform.position.y, transform.position.y);
+        }
+
+        private void OnMouseDown()
+        {
+            if (_button.GetClicked())
+            {
+                (transform.position, _button.transform.position) = (_button.transform.position - _offset, _teleportButtonPosition);
+                Destroy(this);
+            }
+
+            _clicked = true;
+        }
+
+        public Vector3 GetOffset()
+        {
+            return _offset;
+        }
+
+        public Vector3 GetTeleportButtonPosition()
+        {
+            return _teleportButtonPosition;
+        }
+
+        public bool GetClicked()
+        {
+            return _clicked;
+        }
+    }
+}
