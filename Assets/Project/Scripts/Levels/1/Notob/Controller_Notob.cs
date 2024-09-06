88c98ce552eaadd95638273e66b87a1927104aa1
pau <pausoltor@gmail.com>
2024-09-06 10:20:44 +0200

    Add language to notob level

--- Assets/Project/Scripts/Levels/1/Notob/Controller_Notob.cs
+++ Assets/Project/Scripts/Levels/1/Notob/Controller_Notob.cs
@@ -9,11 +9,12 @@ namespace Project.Scripts.Levels._1.Notob
     public class Controller_Notob : MonoBehaviour
     {
         private const String DISCOVERY = "Discovery";
-        
+
         [SerializeField] private Letter_Notob[] _letters;
 
         [SerializeField] private TextMeshProUGUI[] _lettersImage;
 
+        [SerializeField] private GameObject[] _lettersLanguage;
         [SerializeField] private GameObject _button;
 
         [SerializeField] private SpriteRenderer _buttonSpriteRenderer;
@@ -24,9 +25,35 @@ namespace Project.Scripts.Levels._1.Notob
         {
             _audioSource = _button.AddComponent<AudioSource>();
             AudioManager.Instance.SetAudioSourceComponent(_audioSource, DISCOVERY);
+
+            ChangeLanguageLetters(PlayerPrefs.GetString("language"));
         }
 
-        public void CheckOrder() 
+        void ChangeLanguageLetters(string language)
+        {
+            switch (language)
+            {
+                case "es":
+                    ActivateLanguageLetters(0);
+                    break;
+                case "en":
+                    ActivateLanguageLetters(1);
+                    break;
+                default:
+                case "cat":
+                    ActivateLanguageLetters(2);
+                    break;
+            }
+        }
+
+        void ActivateLanguageLetters(int languagePosition)
+        {
+            _lettersLanguage[languagePosition].SetActive(true);
+            _letters = _lettersLanguage[languagePosition].GetComponentsInChildren<Letter_Notob>();
+            _lettersImage = _lettersLanguage[languagePosition].GetComponentsInChildren<TextMeshProUGUI>();
+        }
+
+        public void CheckOrder()
         {
             for (int i = 0; i < _letters.Length - 1; i++)
             {
@@ -48,25 +75,25 @@ namespace Project.Scripts.Levels._1.Notob
         private IEnumerator FadeOutLetter(TextMeshProUGUI image, int index)
         {
             Color color = image.color;
-            
+
             float auxAlpha = color.a;
-            
+
             while (auxAlpha > 0f)
             {
                 auxAlpha -= Time.deltaTime;
                 image.color = new Color(color.r, color.g, color.b, auxAlpha);
                 yield return null;
             }
-            
+
             Destroy(_letters[index].gameObject);
         }
 
         private IEnumerator FadeInButton()
         {
             Color color = _buttonSpriteRenderer.color;
-            
+
             float auxAlpha = color.a;
-            
+
             while (auxAlpha < 1f)
             {
                 auxAlpha += Time.deltaTime;
